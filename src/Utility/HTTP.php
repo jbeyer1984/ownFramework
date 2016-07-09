6c91ef85769947800690316bf718026f437bd4a3
Jens <JensBreed@gmx.de>
2016-07-09 22:45:41 +0200

    route fix for relative routes

--- src/Utility/HTTP.php
+++ src/Utility/HTTP.php
@@ -22,13 +22,16 @@ class HTTP
 
   public static function redirect($path)
   {
-    $path = str_replace('/index.php/', '', $path);
+    $path = str_replace('.*/index.php/', '', $path);
     $path = str_replace('index.php/', '', $path);
     $host = $_SERVER['HTTP_HOST'];
+    
+    $url = self::getRedirectUri($path);
+    
     if ('get' == strtolower($_SERVER['REQUEST_METHOD']) || !isset($_POST['ajaxCall'])) {
-      header('Location: http://'.$host.'/index.php/'.$path);
+      header('Location: http://'.$host.$url);
     } elseif (isset($_POST['ajaxCall'])) {
-      header('Location: http://'.$host.'/index.php/'.$path.'?ajax=true');
+      header('Location: http://'.$host.$url.'?ajax=true');
     }
     die();
   }
@@ -43,5 +46,31 @@ class HTTP
       }
     }
     return self::$method;
+  }
+  
+  private static function getRedirectUri($path = '')
+  {
+    $uri = $_SERVER['REQUEST_URI'];
+    
+    $uriPieces = explode('/', $uri);
+    $pathPieces = explode('/', $path);
+
+    $newUri = array();
+    $foundPiece = 0;
+    foreach ($uriPieces as $key => $piece) {
+      $newUri[] = $piece;
+      
+      if ('index.php' == $piece) {
+        break;
+      }
+      
+      $foundPiece++;
+    }
+    
+    foreach ($pathPieces as $key => $piece) {
+      $newUri[] = $piece; 
+    }
+    
+    return implode('/', $newUri);
   }
 }
